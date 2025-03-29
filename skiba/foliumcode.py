b559bcbe7ad15dd3063284af4d588cae479a9ea8
taraskiba <tskiba@vols.utk.edu>
2025-03-28 22:46:58 -0400

    more work

--- skiba/foliumcode.py
+++ skiba/foliumcode.py
@@ -1,44 +1,19 @@
-"""This module provides a custom Map class that extends folium.Map."""
-
-import os
 import folium
 import folium.plugins
 
 
 class Map(folium.Map):
-    def __init__(self, center=[40, 100], zoom=4, **kwargs):
+    """A custom Map class that extends folium.Map."""
+
+    def __init__(self, center=(0, 0), zoom=2, **kwargs):
+        """Initializes the Map object.
+
+        Args:
+            center (tuple, optional): The initial center of the map as (latitude, longitude). Defaults to (0, 0).
+            zoom (int, optional): The initial zoom level of the map. Defaults to 2.
+            **kwargs: Additional keyword arguments for the folium.Map class.
+        """
         super().__init__(location=center, zoom_start=zoom, **kwargs)
-
-    def add_basemap(self, basemap="OpenStreetMap"):
-        """Add basemap to the map.
-
-        Args:
-            basemap (str, optional): Basemap name. Starts with "OpenStreetMap".
-        """
-
-        url = eval(f"folium.basemaps.{basemap}").build_url()
-        layer = folium.TileLayer(url=url, name=basemap)
-        self.add(layer)
-
-    def add_google_map(self, map_type="ROADMAP"):
-        """Add Google Map to the map.
-
-        Args:
-            map_type (str, optional): Map type. Defaults to "ROADMAP".
-        """
-        map_types = {
-            "ROADMAP": "m",
-            "SATELLITE": "s",
-            "HYBRID": "y",
-            "TERRAIN": "p",
-        }
-        map_type = map_types[map_type.upper()]
-
-        url = (
-            f"https://mt1.google.com/vt/lyrs={map_type.lower()}&x={{x}}&y={{y}}&z={{z}}"
-        )
-        layer = folium.TileLayer(url=url, name="Google Map")
-        self.add(layer)
 
     def add_geojson(
         self,
@@ -53,7 +28,7 @@ class Map(folium.Map):
             data (str or dict): The GeoJSON data. Can be a file path (str) or a dictionary.
             zoom_to_layer (bool, optional): Whether to zoom to the layer's bounds. Defaults to True.
             hover_style (dict, optional): Style to apply when hovering over features. Defaults to {"color": "yellow", "fillOpacity": 0.2}.
-            **kwargs: Additional keyword arguments for the ipyleaflet.GeoJSON layer.
+            **kwargs: Additional keyword arguments for the folium.GeoJson layer.
 
         Raises:
             ValueError: If the data type is invalid.
@@ -68,12 +43,9 @@ class Map(folium.Map):
             geojson = gdf.__geo_interface__
         elif isinstance(data, dict):
             geojson = data
-        layer = folium.GeoJson(data=geojson, hover_style=hover_style, **kwargs)
-        self.add_layer(layer)
 
-        if zoom_to_layer:
-            bounds = gdf.total_bounds
-            self.fit_bounds([[bounds[1], bounds[0]], [bounds[3], bounds[2]]])
+        geojson = folium.GeoJson(data=geojson, **kwargs)
+        geojson.add_to(self)
 
     def add_shp(self, data, **kwargs):
         """Adds a shapefile to the map.
@@ -124,8 +96,7 @@ class Map(folium.Map):
 
     def add_layer_control(self):
         """Adds a layer control widget to the map."""
-        control = folium.LayersControl(position="topright")
-        self.add_control(control)
+        folium.LayerControl().add_to(self)
 
     def add_split_map(self, left="openstreetmap", right="cartodbpositron", **kwargs):
 
