b6903451d28cd940efc155541e5e6b416090b931
Tara Skiba <tskiba@vols.utk.edu>
2025-11-07 21:45:06 -0500

    update function name (#72)

--- skiba/aggregated_point_extraction.py
+++ skiba/aggregated_point_extraction.py
@@ -19,7 +19,7 @@ class aggregated_point_extraction:
         )
         # Dropdown
         url = "https://raw.githubusercontent.com/opengeos/geospatial-data-catalogs/master/gee_catalog.json"
-        data = area_extraction.fetch_geojson(url)
+        data = aggregated_point_extraction.fetch_geojson(url)
         data_dict = {item["title"]: item["id"] for item in data if "title" in item}
         self.dropdown = widgets.Dropdown(
             options=data_dict,  # keys shown, values returned
@@ -54,7 +54,7 @@ class aggregated_point_extraction:
             with self.output:
                 self.output.clear_output()
                 catalog = "https://raw.githubusercontent.com/opengeos/geospatial-data-catalogs/master/gee_catalog.json"
-                data = area_extraction.fetch_geojson(catalog)
+                data = aggregated_point_extraction.fetch_geojson(catalog)
                 data_dict = {item["id"]: item["url"] for item in data if "id" in item}
                 change_value = str(change["new"])
                 url = data_dict.get(change_value)
@@ -144,7 +144,9 @@ class aggregated_point_extraction:
         geojson = gdf.__geo_interface__
         fc = gm.geojson_to_ee(geojson)
         # Load the GEE dataset as an image
-        geeimage = area_extraction.load_gee_as_image(geedata, start_date, end_date)
+        geeimage = aggregated_point_extraction.load_gee_as_image(
+            geedata, start_date, end_date
+        )
         name = f"{geedata}"
         file_name = name.replace("/", "_")
         out_dir = os.path.join(os.path.expanduser("~"), "Downloads")
@@ -186,7 +188,7 @@ class aggregated_point_extraction:
         """
 
         url = "https://raw.githubusercontent.com/opengeos/geospatial-data-catalogs/master/gee_catalog.json"
-        data = area_extraction.fetch_geojson(url)
+        data = aggregated_point_extraction.fetch_geojson(url)
         data_dict = {item["title"]: item["id"] for item in data if "title" in item}
         dropdown = widgets.Dropdown(
             options=data_dict,  # keys shown, values returned
